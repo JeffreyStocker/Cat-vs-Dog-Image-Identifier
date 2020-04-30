7aa6d96cef226c491037c4b888dce6cc8e632382
Jeffrey Stocker <kagesennin@gmail.com>
2020-04-30 15:36:02 -0700

    added default save time for saving models

--- model_save.py
+++ model_save.py
@@ -1,15 +1,16 @@
 import torch
 import os
+import datetime
 
 def save_universal_model(model, save_data, epochs = None, extra_text=None, include_epoch_in_title=None):
-
+    time = datetime.datetime.now()
     checkpoint = save_data
     checkpoint["state_dict"] = model.state_dict()
 
     if epochs:
         if epochs: checkpoint["epochs"] = epochs
 
-    file_name = f'{save_data["model_name"]}_checkpoints_{start_time}{f"_e{epochs}" if include_epoch_in_title else ""}{"-" + extra_text if extra_text else ""}.pth'.replace(":", "-")
+    file_name = f'{save_data["model_name"]}_checkpoints_{time}{f"_e{epochs}" if include_epoch_in_title else ""}{"-" + extra_text if extra_text else ""}.pth'.replace(":", "-")
 
     try:
         torch.save(checkpoint, 'checkpoint/' + file_name)
