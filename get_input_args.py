0e178df1e20bbbf6715133db8e2551b76842128e
Jeffrey Stocker <kagesennin@gmail.com>
2020-05-01 09:08:16 -0700

    set input args for training

--- get_input_args.py
+++ get_input_args.py
@@ -84,19 +84,19 @@ def get_input_args_for_training():
   # you created with this function
   parser = argparse.ArgumentParser(description='Help')
 
-  parser.add_argument('--arch', default='vgg')
-  parser.add_argument('--device', default='cpu')
+  parser.add_argument('--arch', default='resnet101', type=str)
+  parser.add_argument('--device', default='cpu', type=str, choices=['cpu', 'cuda'])
   parser.add_argument('--checkpoint', default=None)
 
-  parser.add_argument('--learning_rate', default='0.003')
-  parser.add_argument('--dropout', default='0.02')
-  parser.add_argument('--n_hidden_layers', default=1)
-  parser.add_argument('--epochs', default='1')
+  parser.add_argument('--learning_rate', default=0.003, type=int, help="Should be a string with each layer sharing before and after, IE 3 layers would have '5, 3, 2, 6'") #
+  parser.add_argument('--dropout', default=0.02, type=int)
+  parser.add_argument('--n_hidden_layers', type=str, default='1, 2, 3')
+  parser.add_argument('--epochs', default=1, type=int)
 
 
-  parser.add_argument('--save_location', default='checkpoint/')
+  parser.add_argument('--save_location', type=str, default='checkpoint/')
   parser.add_argument('--images_path', type=str, default='flowers/')
-  parser.add_argument('--idx_to_names', default='cat_to_name.json')
+  parser.add_argument('--idx_to_names', type=str, default='cat_to_name.json')
 
   return parser
 
