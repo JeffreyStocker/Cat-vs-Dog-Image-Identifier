eb6de01ecb3bfb485a5b8ccdd54aa117dd088c76
Jeffrey Stocker <kagesennin@gmail.com>
2020-05-02 08:33:54 -0700

    checkpoint save less info in title

--- model_save.py
+++ model_save.py
@@ -4,14 +4,14 @@ import datetime
 
 time = datetime.datetime.now()
 
-def save_universal_model(model, save_data, epochs = None, extra_text=None, include_epoch_in_title=None):
+def save_universal_model(model, save_data, epochs = None):
     checkpoint = save_data
     checkpoint["state_dict"] = model.state_dict()
 
     if epochs:
-        if epochs: checkpoint["epochs"] = epochs
+        checkpoint["epochs"] = epochs
 
-    file_name = f'{save_data["model_name"]}_checkpoints_{time}{f"_e{epochs}" if include_epoch_in_title else ""}{"-" + extra_text if extra_text else ""}.pth'.replace(":", "-")
+    file_name = f'{save_data["model_name"]}_checkpoints_{time}.pth'.replace(":", "-").replace(" ", "_")
 
     try:
         torch.save(checkpoint, 'checkpoint/' + file_name)
