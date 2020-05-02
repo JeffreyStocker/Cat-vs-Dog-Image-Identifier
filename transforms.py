dbcd73bd3402bc12a28a329b1dc7620ef75a247f
Jeffrey Stocker <kagesennin@gmail.com>
2020-05-02 10:25:40 -0700

    added training transforms

--- transforms.py
+++ transforms.py
@@ -6,3 +6,12 @@ data_transforms = transforms.Compose([
                                     transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                     ])
+
+data_transforms_train = transforms.Compose([
+                                    transforms.RandomVerticalFlip(),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.RandomRotation(30),
+                                    transforms.RandomResizedCrop(224),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                                    ])
